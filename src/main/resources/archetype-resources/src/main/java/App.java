7b9ecb02f79cefca7c18cf64518d34a26634a2ee
Bruno Andrade <bruno@artsman.dev>
2021-05-23 17:08:09 -0300

    Doing the formatting

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -2,12 +2,11 @@ package $package;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
 }
